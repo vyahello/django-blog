16c19f7fa711da7cd8d811eb5295ab0c226c1e75
Volodymyr Yahello <vyahello@gmail.com>
2022-02-05 13:27:36 +0200

    Add comment-related views

--- web_site/blog/views.py
+++ web_site/blog/views.py
@@ -1,6 +1,8 @@
 from blog.forms import CommentForm, PostForm
 from blog.models import Comment, Post
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views.generic import (
@@ -72,3 +74,40 @@ class DraftListView(LoginRequiredMixin, ListView):
         return Post.objects.filter(published_date__isnull=True).order_by(
             'created_date'
         )
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
--- web_site/web_site/urls.py
+++ web_site/web_site/urls.py
@@ -40,4 +40,24 @@ urlpatterns = [
         views.DraftListView.as_view(),
         name='post_draft_list',
     ),
+    url(
+        r'^post/(?P<pk>\d+)/comment/$',
+        views.add_comment_to_post,
+        name='add_comment_to_post',
+    ),
+    url(
+        r'^comment/(?P<pk>\d+)/approve/$',
+        views.comment_approve,
+        name='comment_approve',
+    ),
+    url(
+        r'^comment/(?P<pk>\d+)/remove/$',
+        views.comment_remove,
+        name='comment_remove',
+    ),
+    url(
+        r'^post/(?P<pk>\d+)/publish/$',
+        views.post_publish,
+        name='post_publish',
+    ),
 ]
