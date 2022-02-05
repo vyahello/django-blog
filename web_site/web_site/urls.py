6b55899f504de76fb086fa15e5679373c66e2fc1
Volodymyr Yahello <vyahello@gmail.com>
2022-02-05 13:59:31 +0200

    Add login html template

--- web_site/web_site/urls.py
+++ web_site/web_site/urls.py
@@ -13,51 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from blog import views
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^$', views.PostListView.as_view(), name='post_list'),
-    url(r'^about/$', views.AboutView.as_view(), name='about'),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', views.login, name='login'),
     url(
-        r'^post/(?P<pk>\d+)$',
-        views.PostDetailView.as_view(),
-        name='post_detail',
-    ),
-    url(r'^post/new/$', views.CreatePostView.as_view(), name='post_new'),
-    url(
-        r'^post/(?P<pk>\d+)/edit/$',
-        views.PostUpdateView.as_view(),
-        name='post_edit',
-    ),
-    url(
-        r'^post/(?P<pk>\d+)/remove/$',
-        views.PostDeleteView.as_view(),
-        name='post_remove',
-    ),
-    url(
-        r'^drafts/$',
-        views.DraftListView.as_view(),
-        name='post_draft_list',
-    ),
-    url(
-        r'^post/(?P<pk>\d+)/comment/$',
-        views.add_comment_to_post,
-        name='add_comment_to_post',
-    ),
-    url(
-        r'^comment/(?P<pk>\d+)/approve/$',
-        views.comment_approve,
-        name='comment_approve',
-    ),
-    url(
-        r'^comment/(?P<pk>\d+)/remove/$',
-        views.comment_remove,
-        name='comment_remove',
-    ),
-    url(
-        r'^post/(?P<pk>\d+)/publish/$',
-        views.post_publish,
-        name='post_publish',
+        r'^accounts/logout/$',
+        views.logout,
+        name='logout',
+        kwargs={'next_page': '/'},
     ),
 ]
